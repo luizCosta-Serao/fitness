8464fb78c510af97240731351958caece0052f90
Luiz Serão <luiz.serao@hotmail.com>
2024-02-08 09:26:46 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <React.Fragment>
+      <BrowserRouter>
+        <div className='container'>
+          <Header />
+          <Routes>
+
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </React.Fragment>
   )
 }
 
